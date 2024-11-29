42f0d8cd2af9a2e52918e14ce661526bab6d97ff
Nagaraj Hebbar <nagaraj.hebbar12@gmail.com>
2024-11-29 21:20:12 +0530

    Get todo api for simple-todo service

--- simple-todo-list/todo-service/src/main/java/com/nagaraj/projects/todoservice/repositories/TodoRepository.java
+++ simple-todo-list/todo-service/src/main/java/com/nagaraj/projects/todoservice/repositories/TodoRepository.java
@@ -1,12 +1,12 @@
 package com.nagaraj.projects.todoservice.repositories;
 
 import com.nagaraj.projects.todoservice.domains.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TodoRepository extends CrudRepository<Todo,String> {
+    Optional<Todo> findByTodoId(String todoId);
 }
